9d5e78e579fdc964142d1e76fb093b3c4e1c4fb0
aquaporin <62488516+aquaporin@users.noreply.github.com>
2021-01-13 22:40:03 +0000

    Added problem 3

--- revc.R
+++ revc.R
@@ -1,6 +1,5 @@
 s <- readLines("./input.txt")
 strand <- strsplit(s, "")[[1]]
 rev_strand <- rev(strand)
-
 rev_string <- paste(rev_strand, collapse='')
-rev_string
+final <- chartr("ATCG", "TAGC", rev_string)
